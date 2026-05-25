efb21da87dfe1c4237f0c9a0760c98dc575dee76
Nhlakanipho2108 <Nhlakanipholuthuli2108@gmail.com>
2026-05-25 12:04:32 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+// Create a variable called score and give it a value between 0 and 100
+let score =100;
+
+// Create a variable called passingGrade and set it to 60
+const passingGrade = 60;
+
+// Check if the score is perfect
+if (score === 100) {
+    console.log("Perfect score!");
+}
+// Check if the score is greater than or equal to passingGrade
+else if (score >= passingGrade) {
+    console.log("Passed");
+}
+// If the score is below passingGrade
+else {
+    console.log("Failed");
+}
